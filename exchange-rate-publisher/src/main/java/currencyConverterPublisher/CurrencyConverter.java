ce1bb9058eceeddc527d31e056ce2b6c0c59bfed
Bruno Campoi <brunocampoi31@gmail.com>
2024-02-18 23:57:48 +0100

    feat: receive API request and convert to JSON
    Create a response object to receive the response from the API and turn it into a JSON object.

--- exchange-rate-publisher/src/main/java/currencyConverterPublisher/CurrencyConverter.java
+++ exchange-rate-publisher/src/main/java/currencyConverterPublisher/CurrencyConverter.java
@@ -1,18 +1,57 @@
 package currencyConverterPublisher;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class CurrencyConverter {
     HttpClient httpClient = null;
     HttpRequest httpRequest = null;
 
-    public CurrencyConverter(){
+    public CurrencyConverter(String baseCurrency) throws IOException, InterruptedException, JSONException {
+        String api_url = getAPI_URL(baseCurrency);
+
+        //Cliente para fazer as solicitações HTTP
         httpClient = HttpClient.newHttpClient();
-        //httpRequest = HttpRequest.newBuilder()
-        //        .uri(URI.create(API_URL))
-        //        .build();
+
+        //Cria o objeto que representa a solicitação que será enviada
+        httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(api_url))
+                .timeout(Duration.of(30, SECONDS))
+                .GET()
+                .build();
+
+        //Envia a solicitação e recebe a resposta
+        //HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        //JSONObject jsonResponse = new JSONObject(response.body());
+
+        String sampleResponse = "{\n" +
+                " \"result\":\"success\",\n" +
+                " \"documentation\":\"https://www.exchangerate-api.com/docs\",\n" +
+                " \"terms_of_use\":\"https://www.exchangerate-api.com/terms\",\n" +
+                " \"time_last_update_unix\":1708214401,\n" +
+                " \"time_last_update_utc\":\"Sun, 18 Feb 2024 00:00:01 +0000\",\n" +
+                " \"time_next_update_unix\":1708300801,\n" +
+                " \"time_next_update_utc\":\"Mon, 19 Feb 2024 00:00:01 +0000\",\n" +
+                " \"base_code\":\"BRL\",\n" +
+                " \"conversion_rates\":{\n" +
+                "  \"BRL\":1,\n" +
+                "  \"EUR\":0.1867,\n" +
+                "  \"USD\":0.2011\n" +
+                " }\n" +
+                "}";
+
+
+        JSONObject jsonResponse = new JSONObject(sampleResponse);
+        System.out.println(jsonResponse);
     }
 
     private String getAPI_URL(String baseCurrency){
--- exchange-rate-publisher/src/main/java/currencyConverterPublisher/Main.java
+++ exchange-rate-publisher/src/main/java/currencyConverterPublisher/Main.java
@@ -1,11 +1,20 @@
 package currencyConverterPublisher;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.http.HttpTimeoutException;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        CurrencyConverter currencyConverter = new CurrencyConverter();
+        try {
+            CurrencyConverter currencyConverter = new CurrencyConverter("BRL");
+        }catch (IOException | InterruptedException | JSONException ex){
+            System.out.println("Não foi possível obter a taxa de câmbio atual. Verifique a conexão com a internet! " + ex);
+        }
+
 
 
     }
